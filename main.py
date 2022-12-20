0156354412d259ed841400cfd58ffc695354d8fa
Eddie Miranda <87393937+EddieMiranda3610@users.noreply.github.com>
2022-12-20 01:13:01 -0600

    Update main.py
    Rewrote attribute information

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print('Now logged in as {0.user}'.format(client))
-  print('Lauren System Bot Version 0.1.0 Update 1')
+  print('Lauren Announcer Version 0.1.0 Update 1')
   print('(C) 2018 - 2021 Eddie Miranda.')
   print('(C) 2007 - 2021 LaurenZSide.')
   print('No part of this program shall be copied, modified, transmitted, reused, or otherwise distributed without permission from either copyright owner.')
   print('This application is currently under development. If you would like to assist in the process: feel free to message us on Discord.')
   print('Our Discord Handles:')
-  print('EddieMiranda&Keira2000#1959')
+  print('EddieMiranda2000#1959')
   print('LaurenzSide#1609')
 
   @client.event
